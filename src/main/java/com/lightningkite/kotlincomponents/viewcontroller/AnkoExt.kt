ee18db762543c50b618ed62e2a275077149afa3e
jivie <joseph@lightningkite.com>
2015-08-03 13:48:08 -0600

    Start on VCs

--- src/main/java/com/lightningkite/kotlincomponents/viewcontroller/AnkoExt.kt
+++ src/main/java/com/lightningkite/kotlincomponents/viewcontroller/AnkoExt.kt
@@ -1,6 +1,9 @@
 package com.lightningkite.kotlincomponents.viewcontroller
 
 import android.content.Context
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
@@ -8,18 +11,26 @@ import android.widget.RelativeLayout
 /**
  * Created by jivie on 7/16/15.
  */
-public fun ViewController.makeLinearLayout(context: Context, init:LinearLayout.()->Unit):LinearLayout{
+public inline fun ViewController.makeLinearLayout(context: Context, init: LinearLayout.() -> Unit): LinearLayout {
     val layout = LinearLayout(context);
     layout.init();
     return layout;
 }
-public fun ViewController.makeFrameLayout(context: Context, init:FrameLayout.()->Unit):FrameLayout{
+
+public inline fun ViewController.makeFrameLayout(context: Context, init: FrameLayout.() -> Unit): FrameLayout {
     val layout = FrameLayout(context);
     layout.init();
     return layout;
 }
-public fun ViewController.makeRelativeLayout(context: Context, init:RelativeLayout.()->Unit):RelativeLayout{
+
+public inline fun ViewController.makeRelativeLayout(context: Context, init: RelativeLayout.() -> Unit): RelativeLayout {
     val layout = RelativeLayout(context);
+    layout.init();
+    return layout;
+}
+
+public inline fun ViewController.inflate(context: Context, LayoutRes layoutResource: Int, init: View.() -> Unit): View {
+    val layout = LayoutInflater.from(context).inflate(layoutResource, null);
     layout.init();
     return layout;
 }
--- src/main/java/com/lightningkite/kotlincomponents/viewcontroller/ViewControllerStack.kt
+++ src/main/java/com/lightningkite/kotlincomponents/viewcontroller/ViewControllerStack.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 public interface ViewControllerStack {
     public fun pushView(newController: ViewController, onResult: (result: Any?) -> Unit = {})
     public fun popView()
-    public fun startIntent(intent: Intent, onResult: (result: Int, data: Intent?) -> Unit = { a, b -> }, options: Bundle = Bundle.EMPTY)
+    public fun startIntent(intent: Intent, onResult: (result: Int, data: Intent?) -> Unit, options: Bundle)
+    public fun startIntent(intent: Intent, onResult: (result: Int, data: Intent?) -> Unit = { resultCode, data -> }) {
+        startIntent(intent, onResult, Bundle.EMPTY)
+    }
 }
 
 public data class ViewControllerData(val controller: ViewController, var onResult: (result: Any?) -> Unit = {}) : ViewController by controller

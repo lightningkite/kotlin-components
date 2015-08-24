54ed8cbcc4e763dd14593110be9fcf693cc80c32
jivie <joseph@lightningkite.com>
2015-08-24 09:58:21 -0600

    Edit Walk View mostly done

--- src/main/java/com/lightningkite/kotlincomponents/LanguageExtensions.kt
+++ src/main/java/com/lightningkite/kotlincomponents/LanguageExtensions.kt
@@ -1,5 +1,8 @@
 package com.lightningkite.kotlincomponents
 
+import java.util.Calendar
+import java.util.Date
+
 /**
  * Created by jivie on 8/7/15.
  */
@@ -16,4 +19,10 @@ fun <E> Collection<E>.stringJoin(separator: String, toStringFunc: (E) -> String)
     }
     builder.setLength(builder.length() - separator.length())
     return builder.toString()
+}
+
+public fun Date.toCalendar(): Calendar {
+    val cal = Calendar.getInstance()
+    cal.setTime(this)
+    return cal
 }

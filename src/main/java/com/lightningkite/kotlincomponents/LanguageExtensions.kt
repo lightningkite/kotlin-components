ff8dabdb1a0d28ae63baf44d8516e7a60791102b
jivie <joseph@lightningkite.com>
2015-08-24 14:05:11 -0600

    Events view controller refactored

--- src/main/java/com/lightningkite/kotlincomponents/LanguageExtensions.kt
+++ src/main/java/com/lightningkite/kotlincomponents/LanguageExtensions.kt
@@ -1,5 +1,6 @@
 package com.lightningkite.kotlincomponents
 
+import java.util.ArrayList
 import java.util.Calendar
 import java.util.Date
 
@@ -21,8 +22,47 @@ fun <E> Collection<E>.stringJoin(separator: String, toStringFunc: (E) -> String)
     return builder.toString()
 }
 
+fun <A, B> Collection<A>.mapOptional(toOtherFunc: (A) -> B?): ArrayList<B> {
+    val results = ArrayList<B>()
+    for (item in this) {
+        val other = toOtherFunc(item)
+        if (other != null) {
+            results.add(other)
+        }
+    }
+    return results
+}
+
 public fun Date.toCalendar(): Calendar {
     val cal = Calendar.getInstance()
     cal.setTime(this)
     return cal
 }
+
+public var Calendar.year: Int
+    get() = get(Calendar.YEAR)
+    set(value) = set(Calendar.YEAR, value)
+
+public var Calendar.month: Int
+    get() = get(Calendar.MONTH)
+    set(value) = set(Calendar.MONTH, value)
+
+public var Calendar.dayOfMonth: Int
+    get() = get(Calendar.DAY_OF_MONTH)
+    set(value) = set(Calendar.DAY_OF_MONTH, value)
+
+public var Calendar.dayOfWeek: Int
+    get() = get(Calendar.DAY_OF_WEEK)
+    set(value) = set(Calendar.DAY_OF_WEEK, value)
+
+public var Calendar.hourOfDay: Int
+    get() = get(Calendar.HOUR_OF_DAY)
+    set(value) = set(Calendar.HOUR_OF_DAY, value)
+
+public var Calendar.minute: Int
+    get() = get(Calendar.MINUTE)
+    set(value) = set(Calendar.MINUTE, value)
+
+public var Calendar.second: Int
+    get() = get(Calendar.SECOND)
+    set(value) = set(Calendar.SECOND, value)

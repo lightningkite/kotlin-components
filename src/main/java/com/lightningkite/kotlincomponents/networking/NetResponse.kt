1dc449317175a9368a6e3406794b7e6471ff94f5
jivie <joseph@lightningkite.com>
2015-09-23 18:00:00 -0600

    Restructured the login

--- src/main/java/com/lightningkite/kotlincomponents/networking/NetResponse.kt
+++ src/main/java/com/lightningkite/kotlincomponents/networking/NetResponse.kt
@@ -2,6 +2,9 @@ package com.lightningkite.kotlincomponents.networking
 
 import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import com.lightningkite.kotlincomponents.BasicGson
 
 /**
@@ -9,10 +12,14 @@ import com.lightningkite.kotlincomponents.BasicGson
  */
 public data class NetResponse(
         public val code: Int,
-        public val body: ByteArray
+        public val rawBody: ByteArray
 ) {
     public val isSuccessful: Boolean get() = code / 100 == 2
     public inline fun <reified T : Any> result(gson: Gson = BasicGson.gson): T? {
-        return gson.fromJson<T>(body.toString("UTF-8"))
+        return gson.fromJson<T>(string())
     }
+
+    public fun string(): String = rawBody.toString("UTF-8")
+    public fun jsonElement(): JsonElement = JsonParser().parse(string())
+    public fun jsonObject(): JsonObject = JsonParser().parse(string()) as JsonObject
 }
--- src/main/java/com/lightningkite/kotlincomponents/networking/OkHttpExtensions.kt
+++ src/main/java/com/lightningkite/kotlincomponents/networking/OkHttpExtensions.kt
@@ -1,6 +1,5 @@
 package com.lightningkite.kotlincomponents.networking
 
-import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.lightningkite.kotlincomponents.BasicGson
@@ -10,10 +9,6 @@ import com.squareup.okhttp.RequestBody
 /**
  * Created by jivie on 9/3/15.
  */
-public inline fun <reified T : Any> NetResponse.gsonTo(gson: Gson = BasicGson.gson): T? {
-    return gson.fromJson<T>(body.toString("UTF-8"))
-}
-
 public inline fun <reified T : Any> T.gsonToRequestBody(gson: Gson = BasicGson.gson): RequestBody {
     return RequestBody.create(Networking.JSON, gson.toJson(this))
 }

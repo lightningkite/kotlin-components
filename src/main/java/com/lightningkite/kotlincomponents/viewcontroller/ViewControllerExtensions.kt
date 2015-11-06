d7a99e04e20ab43666c686353bcd4d0388c273fe
UnknownJoe796 <josephivie@gmail.com>
2015-11-06 16:21:26 -0700

    Added convenience functions for creating layouts to make the style more consistent.

---
+++ src/main/java/com/lightningkite/kotlincomponents/viewcontroller/ViewControllerExtensions.kt
@@ -0,0 +1,37 @@
+package com.lightningkite.kotlincomponents.viewcontroller
+
+import com.lightningkite.kotlincomponents.vertical
+import com.lightningkite.kotlincomponents.viewcontroller.implementations.VCActivity
+import org.jetbrains.anko._FrameLayout
+import org.jetbrains.anko._LinearLayout
+import org.jetbrains.anko._RelativeLayout
+
+/**
+ * Various extension functions specifically for ViewControllers.
+ * Created by josep on 11/6/2015.
+ */
+
+fun ViewController.verticalLayout(activity: VCActivity, setup: _LinearLayout.() -> Unit): _LinearLayout {
+    return _LinearLayout(activity).apply {
+        orientation = vertical
+        setup()
+    }
+}
+
+fun ViewController.linearLayout(activity: VCActivity, setup: _LinearLayout.() -> Unit): _LinearLayout {
+    val layout = _LinearLayout(activity)
+    layout.setup()
+    return layout
+}
+
+fun ViewController.frameLayout(activity: VCActivity, setup: _FrameLayout.() -> Unit): _FrameLayout {
+    val layout = _FrameLayout(activity)
+    layout.setup()
+    return layout
+}
+
+fun ViewController.relativeLayout(activity: VCActivity, setup: _RelativeLayout.() -> Unit): _RelativeLayout {
+    val layout = _RelativeLayout(activity)
+    layout.setup()
+    return layout
+}

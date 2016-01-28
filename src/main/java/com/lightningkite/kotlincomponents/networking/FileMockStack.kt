b9760d34913d0b59e82bfbb51ca485ba9cde6b61
shanelk <shane@meosphere.com>
2016-01-28 13:57:17 -0700

    Rewrite for FileMockStack and MockNetStack

--- src/main/java/com/lightningkite/kotlincomponents/networking/FileMockStack.kt
+++ src/main/java/com/lightningkite/kotlincomponents/networking/FileMockStack.kt
@@ -11,7 +11,7 @@ import java.io.*
  *
  */
 
-open class FileMockStack(val context: Context, val responseCodeForUrl:(String) -> Int, val urlToAssetPath:(String, String?)->String) : NetStack {
+open class FileMockStack(val context: Context, val responseCodeForUrl:(String) -> Int, val urlToAssetPath:(String, NetBody?)->String) : NetStack {
 
     fun readTextFile(successCode:Int, assetPath:String): NetResponse {
         try {
@@ -22,23 +22,7 @@ open class FileMockStack(val context: Context, val responseCodeForUrl:(String) -
         }
     }
 
-    override fun syncGet(headers: Headers, url: String): NetResponse {
-        return readTextFile(responseCodeForUrl(url), urlToAssetPath(url, null) + ".get.txt")
-    }
-
-    override fun syncPost(headers: Headers, url: String, body: String): NetResponse {
-        return readTextFile(responseCodeForUrl(url), urlToAssetPath(url, body) + ".post.txt")
-    }
-
-    override fun syncPut(headers: Headers, url: String, body: String): NetResponse {
-        return readTextFile(responseCodeForUrl(url), urlToAssetPath(url, body) + ".put.txt")
-    }
-
-    override fun syncPatch(headers: Headers, url: String, body: String): NetResponse {
-        return readTextFile(responseCodeForUrl(url), urlToAssetPath(url, body) + ".patch.txt")
-    }
-
-    override fun syncDelete(headers: Headers, url: String, body: String): NetResponse {
-        return readTextFile(responseCodeForUrl(url), urlToAssetPath(url, body) + ".delete.txt")
+    override fun sync(method: NetMethod, url: String, body: NetBody, headers: Map<String, String>): NetResponse {
+        return readTextFile(responseCodeForUrl(url), urlToAssetPath(url, body))
     }
 }
--- src/main/java/com/lightningkite/kotlincomponents/networking/MockNetStack.kt
+++ src/main/java/com/lightningkite/kotlincomponents/networking/MockNetStack.kt
@@ -1,30 +1,31 @@
 package com.lightningkite.kotlincomponents.networking
 
-import com.squareup.okhttp.Headers
-import com.squareup.okhttp.RequestBody
-
 /**
  * Created by shanethompson on 1/27/16.
  */
-class MockNetStack(val createMockResponse:(url:String, body:String?) -> NetResponse) : NetStack {
+class MockNetStack(val createMockResponse:(url:String, body:NetBody?) -> NetResponse) : NetStack {
 
-    override fun syncGet(headers: Headers, url: String): NetResponse {
-        return createMockResponse(url, null)
+    override fun sync(method: NetMethod, url: String, body: NetBody, headers: Map<String, String>): NetResponse {
+        throw UnsupportedOperationException()
     }
 
-    override fun syncPost(headers: Headers, url: String, body: String): NetResponse {
-        return createMockResponse(url, body)
-    }
-
-    override fun syncPut(headers: Headers, url: String, body: String): NetResponse {
-        return createMockResponse(url, body)
-    }
-
-    override fun syncPatch(headers: Headers, url: String, body: String): NetResponse {
-        return createMockResponse(url, body)
-    }
-
-    override fun syncDelete(headers: Headers, url: String, body: String): NetResponse {
-        return createMockResponse(url, body)
-    }
+    //    override fun syncGet(headers: Headers, url: String): NetResponse {
+//        return createMockResponse(url, null)
+//    }
+//
+//    override fun syncPost(headers: Headers, url: String, body: String): NetResponse {
+//        return createMockResponse(url, body)
+//    }
+//
+//    override fun syncPut(headers: Headers, url: String, body: String): NetResponse {
+//        return createMockResponse(url, body)
+//    }
+//
+//    override fun syncPatch(headers: Headers, url: String, body: String): NetResponse {
+//        return createMockResponse(url, body)
+//    }
+//
+//    override fun syncDelete(headers: Headers, url: String, body: String): NetResponse {
+//        return createMockResponse(url, body)
+//    }
 }

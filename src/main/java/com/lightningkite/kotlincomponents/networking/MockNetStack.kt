0694626aa7fee385d01b3b8ce4712960eaa60171
shanelk <shane@meosphere.com>
2016-01-28 14:05:06 -0700

    Removed commented code

--- src/main/java/com/lightningkite/kotlincomponents/networking/MockNetStack.kt
+++ src/main/java/com/lightningkite/kotlincomponents/networking/MockNetStack.kt
@@ -8,24 +8,4 @@ class MockNetStack(val createMockResponse:(url:String, body:NetBody?) -> NetResp
     override fun sync(method: NetMethod, url: String, body: NetBody, headers: Map<String, String>): NetResponse {
         return createMockResponse(url, body)
     }
-
-    //    override fun syncGet(headers: Headers, url: String): NetResponse {
-//        return createMockResponse(url, null)
-//    }
-//
-//    override fun syncPost(headers: Headers, url: String, body: String): NetResponse {
-//        return createMockResponse(url, body)
-//    }
-//
-//    override fun syncPut(headers: Headers, url: String, body: String): NetResponse {
-//        return createMockResponse(url, body)
-//    }
-//
-//    override fun syncPatch(headers: Headers, url: String, body: String): NetResponse {
-//        return createMockResponse(url, body)
-//    }
-//
-//    override fun syncDelete(headers: Headers, url: String, body: String): NetResponse {
-//        return createMockResponse(url, body)
-//    }
 }

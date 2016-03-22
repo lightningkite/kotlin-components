de4385a9618852b9a04616409642b73700a02eaf
jivie <joseph@lightningkite.com>
2016-03-22 13:36:52 -0600

    Fixed failing tests

--- src/main/java/com/lightningkite/kotlincomponents/networking/NetEndpoint.kt
+++ src/main/java/com/lightningkite/kotlincomponents/networking/NetEndpoint.kt
@@ -91,6 +91,7 @@ open class NetEndpoint(val netInterface: NetInterface = NetInterface.default, va
                 data?.gsonToNetBody() ?: NetBody.EMPTY,
                 headers
         ) {
+            println(it.string())
             val result = dealWithResult<T>(it, onError)
             if (result != null) onResult(result)
         }

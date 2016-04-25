e0d58cfbac5240c1cbdbd0a7f696f4da0a6be226
shanelk <=>
2016-04-25 09:15:00 -0600

    Added function to set textview compound drawable, added Snackbar extenstions to handle show and dismiss callbacks.

--- src/main/java/com/lightningkite/kotlincomponents/ui/Snackbar.ext.kt
+++ src/main/java/com/lightningkite/kotlincomponents/ui/Snackbar.ext.kt
@@ -19,3 +19,31 @@ fun View.snackbar(text: Int, duration: Int = Snackbar.LENGTH_LONG, init: Snackba
     snack.init()
     snack.show()
 }
+
+fun android.support.design.widget.Snackbar.callback(init: _Snackbar_Callback.() -> Unit) {
+    val callback = _Snackbar_Callback()
+    callback.init()
+    setCallback(callback)
+}
+
+class _Snackbar_Callback : android.support.design.widget.Snackbar.Callback() {
+
+    private var _onShown: ((Snackbar?) -> Unit)? = null
+    private var _onDismissed: ((Snackbar?, Int) -> Unit)? = null
+
+    override fun onShown(snackbar: Snackbar?) {
+        _onShown?.invoke(snackbar)
+    }
+
+    fun onSnackbarShown(listener: (Snackbar?) -> Unit) {
+        _onShown = listener
+    }
+
+    override fun onDismissed(snackbar: Snackbar?, event: Int) {
+        _onDismissed?.invoke(snackbar, event)
+    }
+
+    fun onSnackbarDismissed(listener: (Snackbar?, Int) -> Unit) {
+        _onDismissed = listener
+    }
+}

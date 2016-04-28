e90f0120833e91b89d3f86c4bda02f7bc1f69966
shanelk <=>
2016-04-28 13:34:22 -0600

    Removed some code I ended up not using

--- src/main/java/com/lightningkite/kotlincomponents/networking/PagedEndpoint.kt
+++ src/main/java/com/lightningkite/kotlincomponents/networking/PagedEndpoint.kt
@@ -88,7 +88,6 @@ open class PagedEndpoint<T : Any>(
                     }
                 }
                 list.addAll(result.getAsJsonArray(listKey).map { it.gsonFrom<T>(type)!! })
-                list.onUpdate.forEach { run(it) }
                 if (nextEndpoint == null) {
                     isMoreObservable.set(false)
                 }
@@ -99,8 +98,7 @@ open class PagedEndpoint<T : Any>(
         } else {
             //option only exists for rapid prototyping purposes.  Servers often add pagination late.
             currentEndpoint.get<JsonArray>(onError = onError) { result ->
-                list.addAll(result.map { it.gsonFrom<T>(type)!! })
-                list.onUpdate.forEach { run(it) }
+                list.addAll(result.map { it.gsonFrom<T>(type)!! })git
                 firstLoadFinishedObservable.set(true)
                 nextEndpoint = null
                 pulling = false
